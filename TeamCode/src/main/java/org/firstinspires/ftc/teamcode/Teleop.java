095ae43e4fe9c1c4e0eb24e9189998efa5acd13d
ThePowerRule <mm44928@wdmcs.org>
2018-02-06 10:35:00 -0600

    Major code cleanup
    Several issues with single-driver controls fixed
    Ugly/clumsy code rewritten
    Improved comments
    Telemetry code in back of opmode loop to further optimize code editing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -14,47 +14,44 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 
 public class Teleop extends Team6475Controls {
 
-    //Define opmode
+    boolean singleDriverControlsEnabled = false; //Allows single-driver controls, hopefully always false
+
     @Override
     public void runOpMode() {
-        //This code runs immediately after the "init" button is pressed.
-        //Initialize the hardware
+        // Initialize the hardware
         initializeHardware();
 
-        //Reset the motor modes so the robot doesn't drive erratically
+        // Reset the motor modes so the robot doesn't drive erratically
         leftMotor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightMotor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         relicExtendMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        // Wait for the game to start (driver presses PLAY)\
-        telemetry.addData("Status", "Waiting for play button");
+        // Initialize and retract servos
+        blueColorServo.setPosition(.1);
+        jewelRotationServo.setPosition(.47);
+
+        // Wait for the game to start (driver presses PLAY)
+        telemetry.addData("Status", "Waiting for start boi.");
         telemetry.update();
         waitForStart();
 
-        // run until the end of the match (driver presses STOP)
+        // Run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            //Inform the user of the current time
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Left Trigger", gamepad2.left_trigger);
-            //Control the chassis
+            // Retrieve sensor data from the REV hub's built-in accelerometer
             angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
             double angle = AngleUnit.DEGREES.normalize(AngleUnit.DEGREES.fromUnit(angles.angleUnit, angles.firstAngle));
-            telemetry.addData("angle", angle);
 
-            blueColorServo.setPosition(.1);
-            jewelRotationServo.setPosition(.47);
-
-
-            //Default drive speed is half power
+            // Control the chassis
+            // Default drive speed is half power
             if (gamepad1.right_trigger < .5) {
                 leftMotor.setPower((-gamepad1.left_stick_y + gamepad1.right_stick_x) / 2.0);
                 leftMotor2.setPower((-gamepad1.left_stick_y + gamepad1.right_stick_x) / 2.0);
                 rightMotor2.setPower((-gamepad1.left_stick_y - gamepad1.right_stick_x) / 2.0);
                 rightMotor.setPower((-gamepad1.left_stick_y - gamepad1.right_stick_x) / 2.0);
             }
-            //By holding down the right trigger we can give a boost to full power
+            // Enable full power by holding right trigger
             if (gamepad1.right_trigger > .5) {
                 leftMotor.setPower(-gamepad1.left_stick_y + gamepad1.right_stick_x);
                 leftMotor2.setPower(-gamepad1.left_stick_y + gamepad1.right_stick_x);
@@ -62,111 +59,32 @@ public class Teleop extends Team6475Controls {
                 rightMotor.setPower(-gamepad1.left_stick_y - gamepad1.right_stick_x);
             }
 
-            //By holding down the left trigger we can give a boost to half power
+            // Enable quarter power by holding left trigger
             if (gamepad1.left_trigger > .5) {
                 leftMotor.setPower((-gamepad1.left_stick_y + gamepad1.right_stick_x) / 4.0);
                 leftMotor2.setPower((-gamepad1.left_stick_y + gamepad1.right_stick_x) / 4.0);
                 rightMotor2.setPower((-gamepad1.left_stick_y - gamepad1.right_stick_x) / 4.0);
                 rightMotor.setPower((-gamepad1.left_stick_y - gamepad1.right_stick_x) / 4.0);
             }
-            //Add telemetry data
-            //    telemetry.addData("Lift motor power", liftMotor.getPower());
-            telemetry.addData("Left motor power", leftMotor.getPower());
-            telemetry.addData("Right motor power", rightMotor.getPower());
-            telemetry.addData("Relic motor power", relicExtendMotor.getPower());
 
-            if (gamepad1.b) {
-                blueColorServo.setPosition(.1);
-                jewelRotationServo.setPosition(.47);
+            // Switch driver modes when the first controller's left stick button is pressed
+            if(gamepad1.left_stick_button) {
+                singleDriverControlsEnabled = !singleDriverControlsEnabled;
+                // Lock the program until the user has stopped pressing the button to
+                // prevent driver mode from oscillating
+                while(gamepad1.left_stick_button&&opModeIsActive());
             }
-            boolean easter = false;
-            if(easter && gamepad1.left_stick_button) {easter = true;};
-            if(!easter && gamepad1.left_stick_button) {easter = false;};
-            if (!easter) {
-                //Lifting mechanism
-                liftGlyphs(0);
 
-                if (gamepad2.dpad_up) {
-                    liftGlyphs(1);
-                }
+            // SINGLE DRIVER CONTROLS
+            if(singleDriverControlsEnabled){
+                // Glyph-related code
+                if (gamepad1.dpad_up) liftGlyphs(1);
+                if (gamepad1.dpad_down) liftGlyphs(-1);
+                else liftGlyphs(0);
+                if (gamepad1.right_trigger > .5) grabGlyphs();
+                if (gamepad1.left_trigger > .5) releaseGlyphs();
 
-                if (gamepad2.dpad_down) {
-                    liftGlyphs(-1);
-                }
-
-
-                //Drop Glyphs
-                if (gamepad2.right_trigger > .5) {
-                    grabGlyphs();
-                }
-
-                if (gamepad2.left_trigger > .5) {
-                    releaseGlyphs();
-                }
-
-                //END GAME CONTROLS
-
-                if (-gamepad2.right_stick_y > 0) {
-                    relicExtendMotor.setPower(gamepad2.right_stick_y);
-                    relicRetractMotor.setPower(-.1);
-                }
-                if (-gamepad2.right_stick_y < 0) {
-                    relicExtendMotor.setPower(.1);
-                    relicRetractMotor.setPower(gamepad2.right_stick_y);
-                }
-                if (-gamepad2.right_stick_y == 0) {
-                    relicExtendMotor.setPower(0);
-                    relicRetractMotor.setPower(0);
-                }
-                if (gamepad2.x) {
-                    grabRelic();
-                }
-
-                if (gamepad2.b) {
-                    releaseRelic();
-                }
-
-                if (gamepad2.y) {
-                    liftRelic();
-                }
-
-                if (gamepad2.a) {
-                    dropRelic();
-                }
-
-                //add some debug data
-
-                //Update the telemetry
-                telemetry.update();
-
-
-                //SINGLE DRIVER CONTROLS
-                // So you can drive with only one controller(press both back buttons)
-
-            }
-            //Easter ----------------------------------------------------------------------------------
-            if (easter) {
-
-                if (gamepad1.dpad_up) {
-                    liftGlyphs(1);
-                }
-
-                if (gamepad1.dpad_down) {
-                    liftGlyphs(-1);
-                }
-
-
-                //Drop Glyphs
-                if (gamepad1.right_trigger > .5) {
-                    grabGlyphs();
-                }
-
-                if (gamepad1.left_trigger > .5) {
-                    releaseGlyphs();
-                }
-
-                //END GAME CONTROLS
-
+                //End game controls
                 if (-gamepad1.right_stick_y > 0) {
                     relicExtendMotor.setPower(gamepad1.right_stick_y);
                     relicRetractMotor.setPower(-.1);
@@ -179,33 +97,54 @@ public class Teleop extends Team6475Controls {
                     relicExtendMotor.setPower(0);
                     relicRetractMotor.setPower(0);
                 }
-                if (gamepad1.a) {
-                    grabRelic();
+                if (gamepad1.a) grabRelic();
+                if (gamepad1.x) releaseRelic();
+                if (gamepad1.y) liftRelic();
+                if (gamepad1.b) dropRelic();
+            }// end of single-driver controls
+            // TWO-DRIVER CONTROLS
+            else {
+                // Glyph-related controls
+                if (gamepad2.dpad_up) liftGlyphs(1);
+                if (gamepad2.dpad_down) liftGlyphs(-1);
+                else liftGlyphs(0);
+                if (gamepad2.right_trigger > .5) grabGlyphs();
+                if (gamepad2.left_trigger > .5) releaseGlyphs();
+
+                // End-game controls
+                if (-gamepad2.right_stick_y > 0) {
+                    relicExtendMotor.setPower(gamepad2.right_stick_y);
+                    relicRetractMotor.setPower(-.1);
                 }
-
-                if (gamepad1.x) {
-                    releaseRelic();
+                if (-gamepad2.right_stick_y < 0) {
+                    relicExtendMotor.setPower(.1);
+                    relicRetractMotor.setPower(gamepad2.right_stick_y);
                 }
-
-                if (gamepad1.y) {
-                    liftRelic();
+                if (-gamepad2.right_stick_y == 0) {
+                    relicExtendMotor.setPower(0);
+                    relicRetractMotor.setPower(0);
                 }
+                if (gamepad2.x) grabRelic();
+                if (gamepad2.b) releaseRelic();
+                if (gamepad2.y) liftRelic();
+                if (gamepad2.a) dropRelic();
+            }// end of two-driver controls
 
-                if (gamepad1.b) {
-                    dropRelic();
-                }
+            // Update the telemetry
+            // Inform the user of important debug information
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Left Trigger", gamepad2.left_trigger);
+            telemetry.addData("angle", angle);
+            telemetry.addData("Single-driver controls enabled", singleDriverControlsEnabled);
+            // Add telemetry data related to motors
+            telemetry.addData("Left motor power", leftMotor.getPower());
+            telemetry.addData("Right motor power", rightMotor.getPower());
+            telemetry.addData("Relic motor power", relicExtendMotor.getPower());
+            // Update telemetry
+            telemetry.update();
+        }// end of main teleop loop
 
-                //add some debug data
-
-                //Update the telemetry
-                telemetry.update();
-
-
-                //SINGLE DRIVER CONTROLS
-                // So you can drive with only one controller(press both back buttons)
-            }
-        }
         telemetry.addData("Status", "Done");
         telemetry.update();
-    }
-}
+    }// end of opMode()
+}// end of class
